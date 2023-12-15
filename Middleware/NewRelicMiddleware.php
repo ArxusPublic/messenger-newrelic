76e69a96c89eeda1e8de41c7643ed4f5a0757765
Kevin Mian Kraiker <kevin.kraiker@vissimo.com.br>
2023-12-15 00:30:03 -0300

    Allow using Messenger's Stamps

--- Middleware/NewRelicMiddleware.php
+++ Middleware/NewRelicMiddleware.php
@@ -4,10 +4,12 @@ namespace Arxus\NewrelicMessengerBundle\Middleware;
 
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicManager;
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicTransactionNameManager;
+use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicTransactionStamp;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
 
 class NewRelicMiddleware implements MiddlewareInterface
 {
@@ -33,15 +35,32 @@ class NewRelicMiddleware implements MiddlewareInterface
             return $stack->next()->handle($envelope, $stack);
         }
 
-        $this->newrelicManager->startTransaction();
-        $this->newrelicManager->nameTransaction($this->newrelicTransactionNameManager->getTransactionName($envelope));
-        try {
-            return $stack->next()->handle($envelope, $stack);
-        } catch (HandlerFailedException $e) {
-            $this->newrelicManager->noticeError($e);
-            throw $e;
-        } finally {
-            $this->newrelicManager->endTransaction();
+        if ($envelope->all(ReceivedStamp::class)) {
+            $this->newrelicManager->startTransaction();
+            $this->newrelicManager->nameTransaction(
+                $this->newrelicTransactionNameManager->getTransactionName($envelope)
+            );
+            try {
+                return $stack->next()->handle($envelope, $stack);
+            } catch (HandlerFailedException $e) {
+                $this->newrelicManager->noticeError($e);
+                throw $e;
+            } finally {
+                $this->newrelicManager->endTransaction();
+            }
         }
+
+        if ($envelope->all(NewrelicTransactionStamp::class)) {
+            return $stack->next()->handle($envelope, $stack);
+        }
+
+        return $stack->next()->handle(
+            $envelope->with(
+                new NewrelicTransactionStamp(
+                    $this->newrelicTransactionNameManager->getTransactionName($envelope)
+                )
+            ),
+            $stack
+        );
     }
 }
--- Newrelic/NewrelicTransactionNameManager.php
+++ Newrelic/NewrelicTransactionNameManager.php
@@ -6,13 +6,36 @@ use Symfony\Component\Messenger\Envelope;
 
 class NewrelicTransactionNameManager
 {
+    /**
+     * @var array<class-string, string>
+     */
+    private array $transactionNameRegistry = [];
+
+    /**
+     * @param class-string $class
+     * @param string       $transactionName
+     *
+     * @return $this
+     */
+    public function addTransactionMapping(string $class, string $transactionName): self
+    {
+        $this->transactionNameRegistry[$class] = $transactionName;
+
+        return $this;
+    }
+
     public function getTransactionName(Envelope $envelope): string
     {
+        $stamp = $envelope->last(NewrelicTransactionStamp::class);
+        if (null !== $stamp) {
+            return $stamp->getTransactionName();
+        }
+
         $message = $envelope->getMessage();
         if ($message instanceof NameableNewrelicTransactionInterface) {
             return $message->getNewrelicTransactionName();
         }
 
-        return get_class($message);
+        return $this->transactionNameRegistry[$class = get_class($message)] ?? $class;
     }
 }
